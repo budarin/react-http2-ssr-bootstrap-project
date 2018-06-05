13fecd04fe4ea1eb59ff07e9d41c51b2b0b008d1
budarin <budarin-vv@yandex.ru>
2018-06-05 22:59:19 +0300

    fix jest from crashing

--- src/common/App.test.js
+++ src/common/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-describe('Home Page', () => {
+describe('Home Page test', () => {
     it('renders correctly', () => {
         const tree = renderer.create(<App />).toJSON();
 
