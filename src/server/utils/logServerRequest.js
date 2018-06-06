c23f7e096f2645479242c6d9bd0d3e0f8817295f
budarin <budarin-vv@yandex.ru>
2018-06-06 14:18:41 +0300

    fix typo

--- src/server/utils/logServerRequest.js
+++ src/server/utils/logServerRequest.js
@@ -3,11 +3,11 @@ import http2 from 'http2';
 
 const { HTTP2_HEADER_PATH, HTTP2_HEADER_METHOD } = http2.constants;
 
-function logServerReaquest(headers) {
+function logServerRequest(headers) {
     const fullPath = headers[HTTP2_HEADER_PATH];
     const method = headers[HTTP2_HEADER_METHOD];
 
     console.log('>> Path:', fullPath, '>> Method:', method);
 }
 
-export default logServerReaquest;
+export default logServerRequest;
