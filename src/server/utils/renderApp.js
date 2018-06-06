3473ef4ffffa556774b65d1e1d58b6035bf968c2
budarin <budarin-vv@yandex.ru>
2018-06-06 18:44:42 +0300

    refactoring: support HTTP1 and HTTP2 clients

--- src/server/utils/renderApp.js
+++ src/server/utils/renderApp.js
@@ -16,29 +16,30 @@ function renderApp(req: Object, res: Object) {
     // it's only for demonstration of push technology
     // preload should not be used with push
 
-    res.write(`<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="theme-color" content="#E6492F">
-                <link rel="manifest" href="/manifest.json">
-                <title>React SSR project</title>
-                <meta name="Description" content="Simple SSR React project.">
-                <meta http-equiv="Accept-CH" content="DPR, Viewport-Width, Width, Downlink">
-                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
-                ${isHttp2 ? '' : '<link rel="preload" type="text/css" href="/app.css" as="style" />'}
-                <link rel="stylesheet" type="text/css" href="/app.css" />
-            </head>
-            <body>
-                <div id="app">`);
+    res.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="theme-color" content="#E6492F">
+            <link rel="manifest" href="/manifest.json">
+            <title>React SSR project</title>
+            <meta name="Description" content="Simple SSR React project.">
+            <meta http-equiv="Accept-CH" content="DPR, Viewport-Width, Width, Downlink">
+            <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+            ${isHttp2 ? '' : '<link rel="preload" type="text/css" href="/app.css" as="style" />'}
+            <link rel="stylesheet" type="text/css" href="/app.css" />
+        </head>
+        <body>
+            <div id="app">`);
 
     const appStream = renderToNodeStream(<App />);
 
     appStream.on('end', () => {
         res.end(`</div>
-                <script src="${env.STATIC_URL}client.js" defer></script>
-            </body>
-            <html>`);
+            <script src="${env.STATIC_URL}client.js" defer></script>
+        </body>
+        <html>`);
     });
 
     appStream.pipe(
