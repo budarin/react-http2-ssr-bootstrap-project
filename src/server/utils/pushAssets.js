095e5540431b1ea951396d91996a3386ed31ac70
budarin <budarin-vv@yandex.ru>
2018-06-06 14:56:43 +0300

    fix: push before send html

--- src/server/utils/pushAssets.js
+++ src/server/utils/pushAssets.js
@@ -2,7 +2,7 @@
 import pushAsset from './pushAsset';
 import getFileDescription from './getFileDescription';
 
-function pushAssets(stream: Object) {
+async function pushAssets(stream: Object) {
     // it's unnecessary to push app.css - css applies at runtime in App.js
     // it's only for demonstration of push technology
     pushAsset(stream, getFileDescription('app.css'));
