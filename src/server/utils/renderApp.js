c47be955db824915607bfb256109e12df6ac0249
budarin <budarin-vv@yandex.ru>
2018-06-06 13:26:42 +0300

    add client hints

--- src/server/utils/renderApp.js
+++ src/server/utils/renderApp.js
@@ -23,6 +23,7 @@ function renderApp(stream: Object) {
                 <title>React SSR project</title>
                 <meta name="theme-color" content="#E6492F">
                 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+                <meta http-equiv="Accept-CH" content="DPR, Viewport-Width, Width, Downlink">
                 <meta name="Description" content="Simple SSR React project.">
                 <link rel="manifest" href="/manifest.json">
                 <link rel="stylesheet" type="text/css" href="/app.css"
