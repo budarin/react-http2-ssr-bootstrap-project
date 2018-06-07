87b06a8f314e6e6df113045a9231edfcecb5e790
budarin <budarin-vv@yandex.ru>
2018-06-07 18:48:14 +0300

    add typing

--- src/utils/getEnv.js
+++ src/utils/getEnv.js
@@ -3,6 +3,17 @@ import fs from 'fs';
 import path from 'path';
 import parseJSON from './parseJSON';
 
+export type ENV = {
+    SERVER_PROTOCOL: string,
+    SERVER_PORT: number,
+    SERVER_HOST: string,
+    SERVER_URL: string,
+    STATIC_PROTOCOL: string,
+    STATIC_PORT: number,
+    STATIC_HOST: string,
+    STATIC_URL: string,
+};
+
 const prodEnvPath = '.env.production.json';
 const devEnvPath = '.env.development.json';
 const defaultEnvPath = '.env.json';
@@ -11,7 +22,7 @@ function getParsedEnv(envPath: string): Object {
     return parseJSON(fs.readFileSync(path.resolve(envPath), { encoding: 'utf-8' }));
 }
 
-function getEnv(): Object {
+function getEnv(): ENV {
     if (process.env.NODE_ENV === 'production' && fs.existsSync(prodEnvPath)) {
         return getParsedEnv(prodEnvPath);
     }
