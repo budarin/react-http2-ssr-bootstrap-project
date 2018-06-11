bf19521244ee41bec8f10854420a283f2867b58d
budarin <budarin-vv@yandex.ru>
2018-06-12 01:43:23 +0300

    add browserList

--- src/config/jest/transformer.js
+++ src/config/jest/transformer.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-commonjs */
 /* eslint-disable import/unambiguous */
+const browserList = require('../browserList');
+
 const config = {
     babelrc: false,
     cacheDirectory: '.tmp/client',
@@ -12,7 +14,7 @@ const config = {
                 modules: 'commonjs',
                 useBuiltIns: true,
                 targets: {
-                    browsers: ['Chrome >= 63', 'Firefox >= 58', 'Safari >= 11', 'iOS >= 10.3', 'Edge >= 41'],
+                    browsers: browserList,
                 },
             },
         ],
