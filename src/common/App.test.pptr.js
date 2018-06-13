2a24e8f9587d3de965dfd07cfa01dc0c66394a81
budarin <budarin-vv@yandex.ru>
2018-06-13 21:39:49 +0300

    change test config

--- src/common/App.test.pptr.js
+++ src/common/App.test.pptr.js
@@ -1,13 +1,23 @@
+import env from '../utils/getEnv';
+
+const { PUPPETEER_URL } = env;
+
 describe('Home Page', () => {
     let page;
+    let client;
 
     beforeAll(async () => {
         page = await global.browser.newPage();
+        client = await page.target().createCDPSession();
 
-        await page.goto('https://localhost');
+        // set DisableCache in Network pane in CDT
+        await client.send('Network.setCacheDisabled', { cacheDisabled: true });
+        await page.goto(`${PUPPETEER_URL}`, { waitUntil: 'networkidle0' });
     });
 
     afterAll(async () => {
+        // Clears browser cache.
+        await client.send('Network.clearBrowserCache');
         await page.close();
     });
 
--- src/config/jest/globalSetup.js
+++ src/config/jest/globalSetup.js
@@ -6,19 +6,24 @@ const puppeteer = require('puppeteer');
 
 const width = 1024;
 const height = 768;
+const CACHE_SIZE = 10000000;
+//  выделяем по 10Mb на кэш
+const args = [`--media-cache-size=${CACHE_SIZE}`, `--disk-cache-size=${CACHE_SIZE}`];
+
 const launchProps =
     process.env.SHOW_BROWSER && process.env.SHOW_BROWSER.trim() === 'true'
-        ? {
+        ? (args.push(`--window-size=${width},${height}`),
+          {
               headless: false,
               slowMo: 200,
-              args: [`--window-size=${width},${height}`],
-          }
-        : {};
+              // userDataDir: './.tmp/chrome/user-data-dir',
+              args: args,
+          })
+        : { args: args };
 
 module.exports = async function() {
     const browser = await puppeteer.launch(launchProps);
 
     global.browser = browser;
-    jest.setTimeout(10000);
     fs.writeFileSync(path.resolve('.tmp/jest/wsEndpoint'), browser.wsEndpoint());
 };
