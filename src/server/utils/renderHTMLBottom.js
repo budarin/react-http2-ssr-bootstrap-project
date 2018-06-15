70457215bbf66a2f06d2f27d6f9cd4a618b36fd2
budarin <budarin-vv@yandex.ru>
2018-06-15 10:02:12 +0300

    move scripts to header to early load

--- src/server/utils/renderHTMLBottom.js
+++ src/server/utils/renderHTMLBottom.js
@@ -1,9 +1,7 @@
 // @flow
-import env from '../../utils/getEnv';
 
 function renderHTMLBottom(): string {
     return `</div>
-            <script src="${env.STATIC_URL}client.js" defer></script>
         </body>
         <html>`;
 }
