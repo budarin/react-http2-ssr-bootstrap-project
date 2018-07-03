aac4a3d7d792849db7bc7e3b3d5022435709a65e
vadim_budarin <budarin-vv@yandex.ru>
2018-07-03 15:33:42 +0300

    refactoring

--- src/common/components/renderCoordinates.tsx
+++ src/common/components/renderCoordinates.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { TMouseEventRenderer } from './MouseCoordinates';
 
-const renderCoordinates: TMouseEventRenderer = ({ x, y }) => {
-    return (
-        <div>
-            <span>Mouse coordinates:</span>
-            <br />
-            <span>x = {x}</span>
-            <span>{'   '}</span>
-            <span>y = {y}</span>
-        </div>
-    );
-};
+const renderCoordinates: TMouseEventRenderer = ({ x, y }) => (
+    <div>
+        <span>Mouse coordinates:</span>
+        <br />
+        <span>x = {x}</span>
+        <span>{'   '}</span>
+        <span>y = {y}</span>
+    </div>
+);
 
 export default renderCoordinates;
