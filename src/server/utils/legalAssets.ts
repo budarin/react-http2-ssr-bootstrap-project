b86ab67adf0ed1b3b720f7e14c7f532032f51f63
vadim_budarin <budarin-vv@yandex.ru>
2018-08-29 13:45:46 +0300

    legal assets for diff modes

--- src/server/utils/legalAssets.ts
+++ src/server/utils/legalAssets.ts
@@ -1,10 +1,19 @@
-export const legalAssets = [
-    '/default.css',
-    '/bundle.js',
-    '/favicon.ico',
-    '/manifest.json',
-    '/android-chrome-192x192.png',
-    '/android-chrome-512x512.png',
-    '/react.development.js',
-    '/react-dom.development.js',
-];
+export const legalAssets = __DEV__
+    ? [
+          '/default.css',
+          '/bundle.js',
+          '/favicon.ico',
+          '/manifest.json',
+          '/android-chrome-192x192.png',
+          '/android-chrome-512x512.png',
+          '/react.development.js',
+          '/react-dom.development.js',
+      ]
+    : [
+          '/default.css',
+          '/bundle.js',
+          '/favicon.ico',
+          '/manifest.json',
+          '/android-chrome-192x192.png',
+          '/android-chrome-512x512.png',
+      ];
