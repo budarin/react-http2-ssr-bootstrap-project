6125436d143ab4ad2c3170d366fba0bb52753bf8
vadim_budarin <budarin-vv@yandex.ru>
2018-08-30 03:42:27 +0300

    fix edge version

--- config/browserList.js
+++ config/browserList.js
@@ -1,1 +1,1 @@
-module.exports = ['Chrome >= 63', 'Firefox >= 58', 'Safari >= 11', 'iOS >= 10.3', 'Edge >= 41'];
+module.exports = ['Chrome >= 63', 'Firefox >= 58', 'Safari >= 11', 'iOS >= 10.3', 'Edge >= 14'];
