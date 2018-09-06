d6e140c9777421edf415af2fa0dea012b1cda0fc
vadim_budarin <budarin-vv@yandex.ru>
2018-09-06 22:29:40 +0300

    fix ts config

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
         '**/?(*.)+(spec|test).js?(x)',
         '**/?(*.)+(spec|test).ts?(x)',
     ],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', '.json'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '../../config/jest/mocks/fileMock.js',
--- jest.puppeteer.config.js
+++ jest.puppeteer.config.js
@@ -8,7 +8,7 @@ module.exports = {
         '^.+\\.(js|ts|jsx|tsx)$': '../config/jest/transformer.js',
     },
     testMatch: ['**/?(*.)+(test).pptr.js?(x)'],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', '.json'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '../../config/jest/mocks/fileMock.js',
