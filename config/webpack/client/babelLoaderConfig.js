49823bb828464ea5bc9f00baf786acdeecc62928
vadim_budarin <budarin-vv@yandex.ru>
2018-09-08 20:17:49 +0300

    remove @babel/plugin-proposal-object-rest-spread

--- config/webpack/client/babelLoaderConfig.js
+++ config/webpack/client/babelLoaderConfig.js
@@ -24,7 +24,6 @@ module.exports = {
         'react-hot-loader/babel',
         '@babel/plugin-syntax-dynamic-import',
         '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-object-rest-spread',
         ['babel-plugin-lodash', { id: ['lodash', 'recompose'] }],
         '@babel/plugin-transform-runtime',
     ],
